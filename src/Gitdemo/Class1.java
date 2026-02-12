b082e8185d1c351ac00573a9479e9f52309db404
vinithaurakana <vinithaurakana1996@gmail.com>
2026-02-12 10:13:38 +0530

    class updated and reports

--- src/Gitdemo/Class1.java
+++ src/Gitdemo/Class1.java
@@ -1,14 +1,19 @@
 package Gitdemo;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Class1 {
+	WebDriver driver;
   @Test
-  public void f() {
+  public void Techlearn() {
+	  driver.get("https://www.techlearn.in/");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
   }
 
 }
