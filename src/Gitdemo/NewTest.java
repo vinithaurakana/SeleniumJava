3b8072338ac065c4a3e28877639acb091867ce42
vinithaurakana <vinithaurakana1996@gmail.com>
2026-02-12 09:33:15 +0530

    New class added for Github

---
+++ src/Gitdemo/NewTest.java
@@ -0,0 +1,14 @@
+package Gitdemo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
